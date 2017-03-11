abe0879a85de50749993e4b6be9281b31984d0b4
Özgür Bircan <ozgurbircan@trakya.edu.tr>
2017-03-11 11:23:54 +0300

    Attribute ile örnekler yapıldı

---
+++ src/main/java/attributerequest/RequestServlet.java
@@ -0,0 +1,57 @@
+package attributerequest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestServlet
+ */
+@WebServlet(name="RequestServlet",urlPatterns={"/requestServlet"})
+public class RequestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Student student=new Student("özgür", "bircan", "12");
+		Student student2=new Student("comert", "baldemir", "13");
+		Student student3=new Student("ilker", "ortak", "14");
+		
+		List<Student> list=new ArrayList<Student>();
+		list.add(student);
+		list.add(student2);
+		list.add(student3);
+		request.setAttribute("studentList", list);
+		RequestDispatcher dispatcher=request.getRequestDispatcher("student.jsp");
+		dispatcher.forward(request, response);
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
